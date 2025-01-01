6294219aefc21751f9bcb7587fd79831824fc917
ditinagrawal <ditin.agrawal05@gmail.com>
2025-01-01 13:03:30 +0530

    Ditin Agrawal Portfolio

---
+++ app/blog/page.tsx
@@ -0,0 +1,38 @@
+import { Metadata } from "next";
+import { BiDetail } from "react-icons/bi";
+import Posts from "../components/pages/Posts";
+import Social from "../components/shared/Social";
+import { Slide } from "../animation/Slide";
+import PageHeading from "@/app/components/shared/PageHeading";
+
+export const metadata: Metadata = {
+  title: "Blog | Ditin Agrawal",
+  metadataBase: new URL("https://ditinagrawal.netlify.app/blog"),
+  description: "Read latest stories from Ditin Agrawal's Blog",
+  openGraph: {
+    title: "Blog | Ditin Agrawal",
+    url: "https://ditinagrawal.netlify.app/blog",
+    description: "Read latest stories from Ditin Agrawal's Blog",
+  },
+};
+
+export default async function Blog() {
+  return (
+    <main className="max-w-7xl mx-auto md:px-16 px-6">
+      <PageHeading
+        title="Blog"
+        description="Welcome to my blog domain where I share personal stories about things I've learned, projects I'm hacking on and just general findings. I also write for other publications."
+      >
+        <Social type="publication" />
+      </PageHeading>
+
+      <Slide delay={0.1}>
+        <div className="flex items-center gap-x-3 mb-8">
+          <BiDetail />
+          <h2 className="text-xl font-semibold tracking-tight">Explore All</h2>
+        </div>
+        <Posts />
+      </Slide>
+    </main>
+  );
+}
